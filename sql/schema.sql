b0ea8415705821cb541164aa4d6cece227054fdc
Johan sannemo <johan.sannemo@gmail.com>
2013-04-02 17:24:34 +0200

    Fixed SQL typos

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,28 +1,28 @@
 CREATE TABLE player (
-    player_id serial PRIMARY KEY,
+    playerId serial PRIMARY KEY,
     name text NOT NULL UNIQUE,
     hash char(128) NOT NULL,
     salt char(16) NOT NULL,
     email text NOT NULL,
     banned boolean NOT NULL DEFAULT false,
-    user_since timestamp with time zone NOT NULL
+    userSince timestamp with time zone NOT NULL
 );
 
 CREATE TABLE game (
-    game_id serial PRIMARY KEY,
-    map_id integer NOT NULL,
-    tick_length integer NOT NULL, -- in seconds
-    last_tick timestamp with time zone NOT NULL,
-    started_at timestamp NOT NULL
+    gameId serial PRIMARY KEY,
+    mapId integer NOT NULL,
+    tickLength integer NOT NULL, -- in seconds
+    lastTick timestamp with time zone NOT NULL,
+    startedAt timestamp NOT NULL
 );
 
 CREATE TABLE game_player (
-    game_id REFERENCES game ON DELETE CASCADE,
-    player_id REFERENCES player ON DELETE CASCADE,
+    gameId integer REFERENCES game ON DELETE CASCADE,
+    playerId integer REFERENCES player ON DELETE CASCADE
 );
 
 CREATE TABLE stock (
-    stock_id serial PRIMARY KEY,
+    stockId serial PRIMARY KEY,
     cannonballs integer NOT NULL,
     gold integer NOT NULL,
     wood integer NOT NULL,
@@ -30,18 +30,18 @@ CREATE TABLE stock (
 );
 
 CREATE TABLE ship (
-    ship_id serial PRIMARY KEY,
-    game_id integer REFERENCES game ON DELETE CASCADE,
-    stock_id integer REFERENCES stock ON DELETE CASCADE,
+    shipId serial PRIMARY KEY,
+    gameId integer REFERENCES game ON DELETE CASCADE,
+    stockId integer REFERENCES stock ON DELETE CASCADE,
     class integer NOT NULL, -- ship class
     damage integer NOT NULL,
     owner integer NOT NULL,
-    pos_x integer NOT NULL,
-    pos_y integer NOT NULL
+    posX integer NOT NULL,
+    posY integer NOT NULL
 );
 
 CREATE TABLE island (
-    island_id serial PRIMARY_KEY,
-    game_id integer REFERENCES game ON DELETE CASCADE,
-    stock_id integer REFERENCES stock ON DELETE CASCADE
+    island_id serial PRIMARY KEY,
+    gameId integer REFERENCES game ON DELETE CASCADE,
+    stockId integer REFERENCES stock ON DELETE CASCADE
 );
